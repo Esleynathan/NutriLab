171db4a20881039503e811586c08a1d7777a460a
Esley <esleynathan@hotmail.com>
2023-02-02 01:53:24 -0300

    NutriLab | Configurações Iniciais, Criação Projeto e app Autenticacao, Criação pagina base.html e cadastro.html

---
+++ autenticacao/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def logar(request):
+    return HttpResponse('Logado')
