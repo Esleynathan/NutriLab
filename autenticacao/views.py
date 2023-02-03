e53dc925cc1a438562d58a08cc8dad2a1b51c0cd
Esley <esleynathan@hotmail.com>
2023-02-03 19:32:06 -0300

    Nutrilab | Importando CSS, Adicionando e aplicando method GET/POST, Validações de Cadastro, Salvando Cadastro no Banco

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils import password_is_valid
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')   
+    elif request.method == "POST":        
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+            return redirect('/auth/logar')
+        except:
+            return redirect('/auth/cadastro')
+
+        return HttpResponse('testando')  
 
 def logar(request):
     return HttpResponse('Logado')
