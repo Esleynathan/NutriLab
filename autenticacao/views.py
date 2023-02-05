d33cb22629b2aa57bc2787c8a44283aa50c68be9
Esley <esleynathan@hotmail.com>
2023-02-05 14:54:41 -0300

    Nutrilab | Exibir mensagem caso usuario já cadastrado, Criação Token de autenticacao

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -8,6 +8,8 @@ from django.contrib.messages import constants
 from django.contrib import auth
 import os
 from django.conf import settings
+from .models import Ativacao
+from hashlib import sha256
 
 
 def cadastro(request):
@@ -19,6 +21,12 @@ def cadastro(request):
         email = request.POST.get('email')
         confirmar_senha = request.POST.get('confirmar_senha')
 
+        user = User.objects.filter(username = username)
+
+        if len(user) > 0:
+            messages.add_message(request, constants.ERROR, 'Usuario já cadastrado.')
+            return redirect('/auth/cadastro')
+
         if not password_is_valid(request, senha, confirmar_senha):
             return redirect('/auth/cadastro')
 
@@ -27,6 +35,10 @@ def cadastro(request):
                                             email=email,
                                             password=senha,)
             user.save()
+
+            token = sha256(f"{username}{email}".encode()).hexdigest()
+            ativacao = Ativacao(token=token, user=user)
+            ativacao.save()
 
             path_template = os.path.join(settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
             email_html(path_template, 'Cadastro confirmado', [email,], username=username)
